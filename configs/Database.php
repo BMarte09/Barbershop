a625217b7603404066d2b7a7a67106f4848d6681
Bryan Miguel Marte <138163452+BMarte09@users.noreply.github.com>
2024-07-27 13:57:35 -0400

    PAssremoval

--- configs/Database.php
+++ configs/Database.php
@@ -2,7 +2,7 @@
 class Database {
     private $host = 'localhost';
     private $username = 'root';
-    private $password = '1234';
+    private $password = '';
     private $database = 'barberiabd';
     public $mysqli;
 
